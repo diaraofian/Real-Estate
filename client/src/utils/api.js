af50a7b9c97522eac326b84095f11f138f779fb9
diaraofian <dianaraofian@gamil.com>
2024-10-29 02:54:35 +0430

    vercel url add in api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,8 @@ import { toast } from "react-toastify";
 
 //import here the base url of our server
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api", //serverURL,
+  baseURL: "https://real-estate-smoky-seven.vercel.app/api",
+  //serverURL,
 });
 
 //this fun will get all property
