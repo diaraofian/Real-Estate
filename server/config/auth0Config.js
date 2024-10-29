e4f496caf1cc5e3cad285d63abc6827795a3d6d2
diaraofian <dianaraofian@gamil.com>
2024-10-29 23:20:32 +0430

    changes in auth

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,7 +2,7 @@ import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
   //as mainjsx auduence must be same url
-  audience: "http://localhost:8000",
+  audience: "https://real-estate-h3vk.onrender.com" || "http://localhost:8000",
   //here i use my domain from main
   issuerBaseURL: "https://dev-plmbu63trvbzf5av.us.auth0.com/",
   tokenSigningAlg: "RS256", //as default
