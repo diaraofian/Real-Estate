13a324c4de7ce1f4dd1283a6caa9cf91c99c13fc
diaraofian <dianaraofian@gamil.com>
2024-10-30 16:31:53 +0430

    chng

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -2,9 +2,9 @@ import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
   //as mainjsx auduence must be same url
-  audience: process.env.AUTH0_AUDIENCE || "http://localhost:8000",
+  audience: "http://localhost:8000",
   //here i use my domain from main
-  issuerBaseURL: process.env.AUTH0_ISSUER,
+  issuerBaseURL: "https://dev-plmbu63trvbzf5av.us.auth0.com/",
   tokenSigningAlg: "RS256", //as default
 });
 
