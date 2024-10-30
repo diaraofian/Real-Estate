00056218b4e427d6a8316e8f306598e18c90f0e6
diaraofian <dianaraofian@gamil.com>
2024-10-30 17:22:33 +0430

    backend deploy added

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,8 @@ import { toast } from "react-toastify";
 
 //import here the base url of our server
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  //  "http://localhost:8000/api",
 
   //serverURL,
 });
